3cdc08123a3152fed5e742c8eb4a976dcf7ea23a
Lucas <lucassilva1998@oi.com.br>
2019-05-14 10:15:33 -0300

    Main

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include <cstring>
 #include "empresa.hpp"
 #include "pessoa.hpp"
 #include "endereco.hpp"
@@ -10,11 +11,12 @@ using namespace std;
 
 int main(){
 
-    string rua;
+    /*string rua;
     string bairro;
     int numero;
     string cidade;
 
+    
     cout <<"Digite Rua: ";
     getline(cin,rua);
     cout <<"\nDigite Bairro: ";
@@ -24,50 +26,74 @@ int main(){
     cin.ignore();
     cout <<"\nDigite Cidade: ";
     getline(cin,cidade);
-//-------------------- TESTE EMPRESA -------------------------------------
+cout << "-------------- TESTE EMPRESA --------------------" << endl;
     Empresa* tabajara = new Empresa();
     string nome_empresa;
     int qntFuncionarios;
 
     cout << "\nDigite Nome Empresa: ";
     getline(cin,nome_empresa);
-    cout << "\nDigite quantidade funcionarios: " << endl;
+    cout << "\nDigite quantidade funcionarios: ";
     cin >> qntFuncionarios;
     *tabajara = Empresa(rua,bairro,numero,cidade,nome_empresa,qntFuncionarios);
     tabajara->imprimeEmpresa();
     delete tabajara;
-//------------------ TESTE PESSOA -----------------------------------------
+cout << "-------------- TESTE PESSOA ------------------" << endl;
     Pessoa* Diego = new Pessoa();
     int idade;
     string nome_pessoa;
     
-    cin.ignore();
+    c(in.ignore();
     cout << "\nDigite o nome da Pessoa: ";
     getline(cin,nome_pessoa);
-    cout << "\nDigite a idade da Pessoa: " << endl;
+    cout << "\nDigite a idade da Pessoa: ";
     cin >> idade;
 
     *Diego = Pessoa(idade,nome_pessoa,bairro,cidade,rua,numero);
     Diego->imprimePessoa();
-    delete Diego;
-//------------------ TESTE DESEMPREGADO -----------------------------------------
+    delete Diego;*/
+cout << "------------ TESTE DESEMPREGADO ----------" << endl;
     Desempregado* fudido = new Desempregado();
-    int tempo;
-    string skill;
-    int idade;
-    string nome_pessoa;
+    int i = 0;
 
-    cin.ignore();
-    cout << "Tempo desempregado: ";
-    cin >> tempo;
-    cout << "Skill do desempregado: ";
-    getline(cin,skill);
+    int tempo;
+    char skill[30];
+
+    int idade;
+    char nome_pessoa[30]; 
+
+    FILE* arq = fopen("entradaDes.txt", "r");
+    while(fscanf(arq, "%[A-Z a-z] %i %[A-Z a-z] %i", nome_pessoa, &idade, skill, &tempo) != EOF){
+        cout << nome_pessoa << endl;
+        cout << idade << endl;
+        while(skill[i] != '\0'){
+            if(skill[i] != ' '){
+                cout << skill[i];
+            }
+            else
+                cout << endl;
+            i++;
+        }
+        cout << endl;
+        cout << tempo << endl << endl;
+        //fseek(arq,sizeof(teste),1);
+    }
+    
+
+    /*cin.ignore();
     cout << "Digite o nome da Pessoa: ";
     getline(cin,nome_pessoa);
-    cout << "Digite a idade da Pessoa: " << endl;
-    cin >> idade;    
+    cout << "Digite a idade da Pessoa: ";
+    cin >> idade; 
+    cin.ignore();
+    cout << "Skill do desempregado: ";
+    getline(cin,skill);
+    cout << "Tempo desempregado: ";
+    cin >> tempo;   
 
     *fudido = Desempregado(tempo,skill,idade,nome_pessoa,bairro,cidade,rua,numero);
-    fudido->imprimePessoa();
+    fudido->imprimeDesempregado();*/
+    delete fudido;
+    delete teste;
     return 0;
 }
